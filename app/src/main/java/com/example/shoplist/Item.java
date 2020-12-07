e89b29708ce0ba3e853aeb4a18c4af06a30f005e
amit1021 <amitelbaz1021@gamil.com>
2020-12-07 11:57:59 +0200

    #Update

--- app/src/main/java/com/example/shoplist/Item.java
+++ app/src/main/java/com/example/shoplist/Item.java
@@ -5,6 +5,9 @@ public class Item {
     String name;
     int quantity;
 
+    public Item(){
+
+    }
     public Item(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
